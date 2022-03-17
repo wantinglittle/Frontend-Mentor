4ad42355cd896df2c74c6daacc68177e7170a367
Daniel <100878892+wantinglittle@users.noreply.github.com>
2022-03-17 16:18:02 -0600

    fix

--- Article-Preview/javascript.js
+++ Article-Preview/javascript.js
@@ -1,4 +1,4 @@
-const button = document.querySelector(".share-button");
+const button = document.querySelector(".share-circle");
 const popupBubble = document.querySelector(".share-popup");
 const popupCorner = document.querySelector(".box-corner");
 
@@ -9,3 +9,13 @@ button.addEventListener('click', function(){
 
 })
 
+const popupButtonSC = document.querySelector(".share-circle-sc");
+const popupArrowSC = document.querySelector(".share-arrow-sc");
+
+
+popupButtonSC.addEventListener('click', function(){
+    popupButtonSC.classList.toggle("share-click-circle")
+    popupArrowSC.classList.toggle("share-click-arrow")
+    popupBubble.classList.toggle("popup-group")
+    popupCorner.classList.toggle("popup-group")
+})
