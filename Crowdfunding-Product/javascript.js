8afb33e3d1dbcfd8760de8813b6790fc218c7e17
Daniel <100878892+wantinglittle@users.noreply.github.com>
2022-03-23 21:34:52 -0600

    update

--- Crowdfunding-Product/javascript.js
+++ Crowdfunding-Product/javascript.js
@@ -21,14 +21,14 @@ const selectReward2 = document.querySelector('.bamboo-button')
 const selectReward3 = document.querySelector('.black-button')
 const closeModal = document.querySelector('.close-modal')
 const content = document.querySelector('.content')
-const topone = document.querySelector('.top')
+const topOne = document.querySelector('.top')
 
 document.getElementById("choice4").disabled = true
 
 const showModal = () => {
     modal.classList.add('show-modal')
     content.classList.add('content-dark')
-    topone.classList.add('content-dark')
+    topOne.classList.add('content-dark')
 }
 
 selectReward1.addEventListener('click', showModal)
@@ -38,5 +38,88 @@ selectReward3.addEventListener('click', showModal)
 closeModal.addEventListener('click', () => {
     modal.classList.remove('show-modal')
     content.classList.remove('content-dark')
-    topone.classList.remove('content-dark')
+    topOne.classList.remove('content-dark')
+})
+
+// Appearance of pledge amount sub-box
+const noPledge = document.querySelector('.no-pledge-enter')
+const bambooPledge = document.querySelector('.bamboo-enter-your-pledge')
+const blackPledge = document.querySelector('.black-enter-your-pledge')
+
+const noSelector = document.querySelector('.no-selector')
+const noContainer = document.querySelector('.no-pledge-container')
+const modalNo = document.querySelector('.modal-no-pledge')
+
+const bambooSelector = document.querySelector('.bamboo-selector')
+const bambooContainer = document.querySelector('.bamboo-container')
+const modalBamboo = document.querySelector('.modal-bamboo')
+
+const blackSelector = document.querySelector('.black-selector')
+const blackContainer = document.querySelector('.black-container')
+const modalBlack = document.querySelector('.modal-black')
+
+noSelector.addEventListener('click', () => {
+    blackPledge.classList.remove('enter-your-pledge-appear')
+    blackContainer.classList.remove('add-container-border')
+    modalBlack.classList.remove('change-modal-box-border')
+    
+    bambooPledge.classList.remove('enter-your-pledge-appear')
+    bambooContainer.classList.remove('add-container-border')
+    modalBamboo.classList.remove('change-modal-box-border')
+
+    noPledge.classList.add('enter-your-pledge-appear')
+    noContainer.classList.add('add-container-border')
+    modalNo.classList.add('change-modal-box-border')
+})
+
+bambooSelector.addEventListener('click', () => {
+    noPledge.classList.remove('enter-your-pledge-appear')
+    noContainer.classList.remove('add-container-border')
+    modalNo.classList.remove('change-modal-box-border')
+
+    blackPledge.classList.remove('enter-your-pledge-appear')
+    blackContainer.classList.remove('add-container-border')
+    modalBlack.classList.remove('change-modal-box-border')
+    
+    bambooPledge.classList.add('enter-your-pledge-appear')
+    bambooContainer.classList.add('add-container-border')
+    modalBamboo.classList.add('change-modal-box-border')
+})
+
+blackSelector.addEventListener('click', () => {
+    noPledge.classList.remove('enter-your-pledge-appear')
+    noContainer.classList.remove('add-container-border')
+    modalNo.classList.remove('change-modal-box-border')
+
+    bambooPledge.classList.remove('enter-your-pledge-appear')
+    bambooContainer.classList.remove('add-container-border')
+    modalBamboo.classList.remove('change-modal-box-border')
+    
+    blackPledge.classList.add('enter-your-pledge-appear')
+    blackContainer.classList.add('add-container-border')
+    modalBlack.classList.add('change-modal-box-border')
+})
+
+// After clicking continue, pop up 'thanks' box and update progress info 
+
+const contin = document.querySelectorAll('.continue')
+const thanks = document.querySelector('.thanks-box')
+const gotIt = document.querySelector('.got-it')
+const totalBackers = document.querySelector('.total-backers-top')
+let backers=5007
+let dollarAmount=89914
+
+contin.forEach((c) => {
+    c.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+    thanks.classList.add('show-thanks')
+    })
+})
+
+gotIt.addEventListener('click', () => {
+    thanks.classList.remove('show-thanks')
+    content.classList.remove('content-dark')
+    topOne.classList.remove('content-dark')
+    totalBackers.textContent=(backers+1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    backers++
 })
